f2037297221bd8bd7496bbd6819f667e5613b4b4
noxxxxxious <sean.fried@gmail.com>
2023-06-26 23:33:07 -0500

    add socket.io

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,17 @@
-import express from "express"
-import path from 'path'
 import 'dotenv/config'
+import { createServer } from 'http'
+import express from "express"
+import { Server } from 'socket.io'
+import path from 'path'
+
 
 import createUser from "./database/functions/user/createUser.js"
 
 const __dirname = path.resolve(path.dirname(''))
 
 const app = express()
+const httpServer = createServer(app)
+const io = new Server(httpServer)
 
 app.use(express.static('public'))
 app.use(express.json())
@@ -23,7 +28,11 @@ app.get('/', (_req, res) => {
     res.sendFile(__dirname + '/out/public/index.html')
 })
 
-app.listen(process.env.EXPRESS_PORT, () => {
+io.on('connection', (socket) => {
+    console.log('user connected')
+})
+
+httpServer.listen(process.env.EXPRESS_PORT, () => {
     console.log(`Express running on port ${process.env.EXPRESS_PORT}.`)
     console.log(`MongoDB connection URL: ${process.env.MONGODB_URL}`)
 })
